41a3ca94bc15ec62b60161c13e3599fbd0493a34
Ryan Wang <ryan@ink-titan.usc.edu>
2023-05-01 10:58:13 -0700

    refined extraction

--- OxfordCommaExtract.py
+++ OxfordCommaExtract.py
@@ -1,46 +1,77 @@
 import json
+import argparse
+import os
 import gzip
 from nltk.tokenize import sent_tokenize
 import nltk
 nltk.download('punkt')
 import re
 import csv
-# from tqdm.notebook import tqdm
+import random
 
-print("yay")
-data_train = list(gzip.open('/home/johnny/data/00.jsonl.gz', 'rt'))
 
-#For the train data generation
+OPTS = None
 
-regex = ",([^,]){1,20}(,)?\s(and|or)\s"
+#Used regex expression
+regex = "([^,]{4,15}, ){2,}[^,]{4,15}(,)?\s(and|or)\s"
 
-outName = "train_oxford.csv"
+# outName = "train_oxford.csv"
 
-with open(outName, 'w') as csvfile0: 
-    csvWriter0 = csv.writer(csvfile0)
-    csvWriter0.writerow(["lineInd", "sentence", "hasOxford", "index"])
-    
-    for lineInd in range(3000):
-#         sent_tokenize(re.sub(r'\n\n', ' ', json.loads(data[1])["text"]))
-        tokenized_line = sent_tokenize(re.sub('\n', ' ', json.loads(data_train[lineInd])["text"]))
-        for sentence in tokenized_line:
-            result = re.search(regex, sentence)
-            if (result != None):
-                hasOxford = result.group(2) == ','
-#                 print(result)
-#                 print(result.group(2))
-                isAnd = (result.group(3) == "and")
-                offSet = 6 if isAnd else 5
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', required=True, choices=['train', 'val'])
+    parser.add_argument('--outName', default='out.csv')
+
+    return parser.parse_args()
+
+def main():
+    if (OPTS.source == "train"):
+        fileName = "00.jsonl.gz"
+    else:
+        fileName = "val.jsonl.gz"
+    inFile = os.path.join('/home/johnny/data', fileName)
+    data_train = list(gzip.open(inFile, 'rt'))
+    with open(os.path.join(os.getcwd(), 'out', OPTS.outName), 'w') as csvfile0: 
+        csvWriter0 = csv.writer(csvfile0)
+        csvWriter0.writerow(["lineInd", "sentence", "hasOxford", "index"])
+
+        numPos = 0
+        numNeg = 0
+        lineInd = 0
         
-                
-                index = result.span()[1] - offSet
-                
-                #This is so that our index is always at the position that is supposed to be the comma (for non oxford, this is the space)
-                if (not hasOxford):
-                    index+= 1
-#                 print(result.group(3))
-#                 print(sentence[index])
-    
+        while (numPos + numNeg < 3000):
+    #         sent_tokenize(re.sub(r'\n\n', ' ', json.loads(data[1])["text"]))
+            tokenized_line = sent_tokenize(re.sub('\n', ' ', json.loads(data_train[lineInd])["text"]))
+            #For randomly selecting a place to start searching
+            randomStart = random.randint(0, len(tokenized_line))
+            for sentenceInd in range(len(tokenized_line)):
+                result = re.search(regex, tokenized_line[(sentenceInd + randomStart) % len(tokenized_line)])
+                if (result != None):
+                    hasOxford = result.group(2) == ','
+    #                 print(result)
+    #                 print(result.group(2))
+                    isAnd = (result.group(3) == "and")
+                    offSet = 6 if isAnd else 5
                     
-                csvWriter0.writerow([lineInd, sentence, hasOxford, index])
+                    index = result.span()[1] - offSet
+                    
+                    #This is so that our index is always at the position that is supposed to be the comma (for non oxford, this is the space)
+                    if (not hasOxford):
+                        index+= 1
+    #                 print(result.group(3))
+    #                 print(sentence[index])
         
+                        
+                    csvWriter0.writerow([lineInd, tokenized_line[(sentenceInd + randomStart) % len(tokenized_line)], hasOxford, index])
+                    if (hasOxford):
+                        numPos += 1
+                    else:
+                        numNeg += 1
+                    break
+            lineInd += 1
+    print(f"With oxford comma = {numPos}, without oxford comma = {numNeg}")
+
+if __name__ == '__main__':
+    OPTS = parse_args()
+    main()

1016ef602e0577c71d557d36eed3626c9c9c5ec4
Ryan Wang <ryanyxw@gmail.com>
2023-06-26 13:30:37 -0700

    bash script commit

--- training/minipile/bash_pretrain.sh
+++ training/minipile/bash_pretrain.sh
@@ -7,18 +7,22 @@ TOKENIZED_DIR=./data
 mkdir -p ${MODEL_DIR}
 mkdir -p ${TOKENIZED_DIR}
 
-#python data_process.py\
-#  --context_length 256\
-#  --tokenized_data_dir ${TOKENIZED_DIR}/tokenized_dataset_256\
-#  --tokenize_and_save
+#For data process
+python data_process.py\
+  --context_length 512\
+  --tokenized_data_dir ${TOKENIZED_DIR}/tokenized_dataset_256\
+  --tokenize_and_save
 
+#For actual training
+#accelerate launch train.py\
+#  --context_length 1024\
+#  --weight_decay 0.1\
+#  --learning_rate 5e-4\
+#  --num_train_epochs 1\
+#  --model_output_dir ${MODEL_DIR}/gpt2_1024\
+#  --tokenized_data_dir ${TOKENIZED_DIR}/tokenized_dataset_1024\
+#  --precision fp16
 
-
-accelerate launch train.py\
-  --context_length 1024\
-  --weight_decay 0.1\
-  --learning_rate 5e-4\
-  --num_train_epochs 1\
-  --model_output_dir ${MODEL_DIR}/gpt2_1024\
-  --tokenized_data_dir ${TOKENIZED_DIR}/tokenized_dataset_1024\
-  --precision fp16
+#For query
+#python query.py\
+#  --inference_model ${MODEL_DIR}/gpt2_1024\
